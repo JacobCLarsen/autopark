937bfa01507418ceeeee82d25bbe0f4aee89cd6b
JacobCLarsen <jcla24@student.aau.dk>
2025-06-02 12:49:23 +0200

    website design

---
+++ src/lib/components/generalinfo.svelte
@@ -0,0 +1,54 @@
+<script>
+</script>
+
+<div id="user-info-form" class="grid grid-cols-2 grid-rows-3 gap-5 p-5">
+	<div class="flex flex-col gap-1.5">
+		<label class="text-gray-500" for="name">Navn</label>
+		<input
+			class="col-span-1 rounded border-gray-300 text-gray-800 placeholder:text-gray-400"
+			type="text"
+			placeholder="Michael Rune"
+			name="name"
+		/>
+	</div>
+
+	<div class="flex flex-col gap-1.5">
+		<label class="text-gray-500" for="phone">Telefonnummer</label>
+		<input
+			class="col-span-1 rounded border-gray-300 text-gray-800 placeholder:text-gray-400"
+			type="text"
+			placeholder="+45 12 34 56 78"
+			name="phone"
+		/>
+	</div>
+
+	<div class="flex flex-col gap-1.5">
+		<label class="text-gray-500" for="email">Email</label>
+		<input
+			class="col-span-1 rounded border-gray-300 text-gray-800 placeholder:text-gray-400"
+			type="text"
+			placeholder="email@unipark.dk"
+			name="email"
+		/>
+	</div>
+
+	<div class="flex flex-col gap-1.5">
+		<label class="text-gray-500" for="password">Kodeord</label>
+		<input
+			class="col-span-1 rounded border-gray-300 text-gray-800 placeholder:text-gray-400"
+			type="text"
+			placeholder="* * * * * * *"
+			name="password"
+		/>
+	</div>
+
+	<div class="col-span-2 flex flex-col gap-1.5">
+		<label class="text-gray-500" for="address">Addresse</label>
+		<input
+			class="col-span-1 rounded border-gray-300 text-gray-800 placeholder:text-gray-400"
+			type="text"
+			placeholder="Begynd at skrive ..."
+			name="address"
+		/>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,84 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Navbar from '$lib/components/navbar.svelte';
+	import Generalinfo from '$lib/components/generalinfo.svelte';
+	import Vehicleinfo from '$lib/components/vehicleinfo.svelte';
+	import Payment from '$lib/components/payment.svelte';
+
+	let formpage = $state(0);
+
+	function nextForm() {
+		if (formpage < 2) {
+			formpage += 1;
+		}
+	}
+	function prevForm() {
+		if (formpage > 0) {
+			formpage -= 1;
+		}
+	}
+</script>
+
+<Navbar />
+<section class="mt-40 flex w-full flex-col items-center">
+	<div class="flex flex-col items-center gap-7">
+		<h1 class="text-4xl">Automatisk Registering af Parkering</h1>
+		<p class="text-gray-500">Indtast oplysninger om dig og din bil herunder for at fortsætte</p>
+	</div>
+</section>
+
+<section class="mt-30">
+	<div class="flex flex-col items-center">
+		<div class="rounded-xl border-1 border-gray-200 p-10 shadow-lg">
+			<form action="register">
+				<!-- Info om bruger (navn, email, adresse og kodeord)-->
+				{#if formpage === 0}
+					<Generalinfo />
+				{/if}
+				<!-- Registering af bil -->
+				{#if formpage === 1}
+					<Vehicleinfo />
+				{/if}
+				<!-- Betaling -->
+				{#if formpage === 2}
+					<Payment />
+				{/if}
+				<!-- Buttons -->
+				<div class="mr-5 flex justify-end gap-5">
+					{#if formpage > 0}
+						<button
+							type="button"
+							onclick={() => {
+								prevForm();
+							}}
+							id="form-next-btn"
+							class="px-5 py-2 font-sans tracking-wide text-gray-400 hover:text-gray-600
+						">Tilbage</button
+						>
+					{/if}
+					{#if formpage < 2}
+						<button
+							type="button"
+							onclick={() => {
+								nextForm();
+							}}
+							id="form-prev-btn"
+							class="box-border rounded px-5 py-2 font-sans tracking-wide text-blue-600 outline-1 outline-blue-600 hover:text-blue-700 hover:outline-2 hover:outline-blue-700
+						">Næste</button
+						>
+					{/if}
+					{#if formpage === 2}
+						<button
+							type="button"
+							onclick={() => {
+								console.log('betalt!');
+							}}
+							id="form-pay-btn"
+							class="box-border rounded bg-blue-600 px-5 py-2 font-sans tracking-wide text-white hover:bg-blue-700 hover:text-white
+						">Betal</button
+						>
+					{/if}
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
